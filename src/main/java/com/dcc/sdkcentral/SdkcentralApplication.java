f85a74941806549ad706ca7a6f5aeba1461bd977
vengsrun <vengsrun2017new@gmail.com>
2026-06-30 14:03:56 +0700

    first commit

---
+++ src/main/java/com/dcc/sdkcentral/SdkcentralApplication.java
@@ -0,0 +1,13 @@
+package com.dcc.sdkcentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdkcentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdkcentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dcc/sdkcentral/SdkcentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcc.sdkcentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdkcentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
